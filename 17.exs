6a59f9dcc2b21de8d2fc28c86076e83d5633a1b3
Aristides Kodjas <akodjas@ebay.com>
2024-12-21 01:43:52 +0200

    day17 part1

--- 17.exs
+++ 17.exs
@@ -1,8 +1,5 @@
 import Enum
-import Integer
 import Bitwise
-
-literal_operand = fn x -> x end
 
 combo_operand = fn
   4, a, _b, _c -> a
@@ -12,13 +9,12 @@ combo_operand = fn
 end
 
 adv = fn {i, a, b, c, program, output} ->
-  denominator =
-    2 **
-      (program
-       |> at(i + 1)
-       |> combo_operand.(a, b, c))
+  d =
+    program
+    |> at(i + 1)
+    |> combo_operand.(a, b, c)
 
-  a = a |> floor_div(denominator)
+  a = a >>> d
 
   {i + 2, a, b, c, program, output}
 end
@@ -27,7 +23,6 @@ bxl = fn {i, a, b, c, program, output} ->
   b =
     program
     |> at(i + 1)
-    |> literal_operand.()
     |> bxor(b)
 
   {i + 2, a, b, c, program, output}
@@ -38,26 +33,25 @@ bst = fn {i, a, b, c, program, output} ->
     program
     |> at(i + 1)
     |> combo_operand.(a, b, c)
-    |> mod(8)
+    |> band(7)
 
   {i + 2, a, b, c, program, output}
 end
 
 jnz = fn
   {i, 0, b, c, program, output} ->
-    {i, 0, b, c, program, output}
+    {i + 2, 0, b, c, program, output}
 
   {i, a, b, c, program, output} ->
     i =
       program
       |> at(i + 1)
-      |> literal_operand.()
 
     {i, a, b, c, program, output}
 end
 
 bxc = fn {i, a, b, c, program, output} ->
-  c = bxor(b, c)
+  b = bxor(b, c)
 
   {i + 2, a, b, c, program, output}
 end
@@ -67,31 +61,29 @@ out = fn {i, a, b, c, program, output} ->
     program
     |> at(i + 1)
     |> combo_operand.(a, b, c)
-    |> mod(8)
+    |> band(7)
 
   {i + 2, a, b, c, program, [o | output]}
 end
 
 bdv = fn {i, a, b, c, program, output} ->
-  denominator =
-    2 **
-      (program
-       |> at(i + 1)
-       |> combo_operand.(a, b, c))
+  d =
+    program
+    |> at(i + 1)
+    |> combo_operand.(a, b, c)
 
-  b = a |> floor_div(denominator)
+  b = a >>> d
 
   {i + 2, a, b, c, program, output}
 end
 
 cdv = fn {i, a, b, c, program, output} ->
-  denominator =
-    2 **
-      (program
-       |> at(i + 1)
-       |> combo_operand.(a, b, c))
+  d =
+    program
+    |> at(i + 1)
+    |> combo_operand.(a, b, c)
 
-  c = a |> floor_div(denominator)
+  c = a >>> d
 
   {i + 2, a, b, c, program, output}
 end
@@ -108,40 +100,68 @@ instructions =
     7 -> cdv
   end
 
-program = [0, 1, 5, 4, 3, 0]
-a = 729
-b = 0
-c = 0
-
-# input = %{
-#  registers: { 51064159, 0, 0},
-#  program:  [ 2,4,1,5,7,5,1,6,0,3,4,6,5,5,3,0 ]
-# }
-
-program = [0, 1, 5, 4, 3, 0]
-a = 2024
-b = 0
-c = 0
-
-# program = [2, 6]
-# a = 0
-# b = 0
-# c = 9
-#
-# program = [5, 0, 5, 1, 5, 4]
-# a = 10
+# test1
+# program = [0, 1, 5, 4, 3, 0]
+# a = 729
 # b = 0
 # c = 0
 
-0..10
-|> reduce_while({0, a, b, c, program, []}, fn _, {i, a, b, c, program, output} ->
-  if i < count(program) do
-    {:cont, instructions.(at(program, i)).({i, a, b, c, program, output}) |> dbg}
-  else
-    {:halt, {i, a, b, c, program, output}}
-  end
-end)
-|> then(fn {i, a, b, c, program, output} -> output end)
-|> reverse
-|> join(",")
+# part1 = 3,6,3,7,0,7,0,3,0
+
+a = 51_064_159
+b = 0
+c = 0
+program = [2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 6, 5, 5, 3, 0]
+
+part1 =
+  0..72
+  |> reduce_while({0, a, b, c, program, []}, fn n, {i, a, b, c, program, output} ->
+    cond do
+      i > count(program) - 1 ->
+        # n |> IO.puts()
+        {:halt, {i, a, b, c, program, output}}
+
+      true ->
+        {:cont, instructions.(at(program, i)).({i, a, b, c, program, output})}
+    end
+  end)
+  |> then(fn {i, a, b, c, program, output} -> output end)
+  |> reverse
+  |> join(",")
+  |> IO.inspect()
+
+#######################
+
+# test2
+
+program = [0, 1, 5, 4, 3, 0]
+program = [0, 3, 5, 4, 3, 0]
+
+test = fn n ->
+  result =
+    0..999_999
+    |> reduce_while({0, n, b, c, program, []}, fn _, {i, a, b, c, program, output} ->
+      cond do
+        program |> take(count(output)) != output |> reverse ->
+          {:halt, {:skip, n, reverse(output)}}
+
+        i > count(program) - 1 ->
+          # {:halt, {i, a, b, c, program, output |> reverse}}
+          {:halt, output |> reverse}
+
+        # program == reverse(output) ->
+        # {:halt, {:ayy, n, program, reverse(output)}}
+
+        true ->
+          {:cont, instructions.(at(program, i)).({i, a, b, c, program, output})}
+      end
+    end)
+
+  # result |> dbg(charlists: :as_lists)
+  result == program
+end
+
+0_000_000..1_000_000
+|> Task.async_stream(fn n -> {test.(n), n} end)
+|> find(fn {:ok, {t, _}} -> t == true end)
 |> dbg
